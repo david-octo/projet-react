561033895a8517c4f0334fcab7adc7e977b2099f
wToad <willehkey@mailoo.org>
2020-01-24 14:39:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [beers, setBeers] = useState([])
+  useEffect(() => {
+    fetch("https://api.punkapi.com/v2/beers")
+    .then(response => response.json())
+    .then(setBeers)
+  }, [])
+
+console.log(beers)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {beers.map(beer => <li key={beer.id}><img src={beer.image_url} alt="{beer.name} image" width="8%" height="8%"/> {beer.name} ({beer.tagline})</li>
+            )}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
